50790b2a3ebe018585b80b352df0add692afd131
Afshan Akhtar <akhtaraf@sheridancollege.ca>
2023-07-23 04:23:03 -0400

    Added: Controller and Resources

---
+++ src/main/java/sheridan/akhtaraf/counter/controller/CounterController.java
@@ -0,0 +1,24 @@
+package sheridan.akhtaraf.counter.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class CounterController {
+
+    private final Logger log = LoggerFactory.getLogger(CounterController.class);
+    @GetMapping("/")
+    public ModelAndView counterHome(
+            @RequestParam(defaultValue = "0") int counterValue
+    ){
+        log.trace("Initiated the Counter Controller.");
+        log.debug("CounterValue = " + counterValue);
+        System.out.println("print");
+        return new ModelAndView("index");
+    }
+
+}
